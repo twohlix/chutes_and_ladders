dd7442235821afff0bef7963144c628eff6eb62c
Charles Smith <twohlix@gmail.com>
2015-06-10 13:17:45 -0400

    Added multithreading for rbx/jruby. Added options parsing

--- test.rb
+++ test.rb
@@ -1,4 +1,5 @@
 require_relative 'game.rb'
+require 'optparse'
 
 def puts_histogram(the_array)
   max_value = the_array.max
@@ -11,26 +12,66 @@ def puts_histogram(the_array)
   end
 end
 
-games_to_simulate = 10000000
-puts "Simulating #{games_to_simulate} games of chutes and ladders"
+def simulate_games(games_to_simulate)
+  board = Board.new
+  frequency_of_turns = Array.new 151, 0
+  number_of_wins = games_simulated = 0
 
-board = Board.new
-frequency_of_turns = Array.new 151, 0
-number_of_wins = games_simulated = 0
+  (1..games_to_simulate).each do |game_number|
+    game = Game.new board: board
+    game.simulate
 
-(1..games_to_simulate).each do |game_number|
-  game = Game.new board: board
-  game.simulate
+    games_simulated += 1
 
-  games_simulated += 1
+    if game.won?
+      frequency_of_turns[game.turns] += 1
+      number_of_wins += 1
+    end
 
-  if game.won?
-    frequency_of_turns[game.turns] += 1
-    number_of_wins += 1
+    Thread.current[:simulated_games] = game_number
   end
 
-  puts "simulated #{game_number} games" if game_number % 10000 == 0
+  return { frequency_of_turns: frequency_of_turns, number_of_wins: number_of_wins, games_simulated: games_simulated }
+end
+
+options = { games_to_simulate: 1000000, number_of_threads: 1 }
+opt_parser = OptionParser.new do |opts|
+  opts.banner = "Usage: #{__FILE__} [options]"
+
+  opts.on('-g GAMES', '--games GAMES', Integer, "Number of games to simulate [#{options[:games_to_simulate]}]") do |g|
+    options[:games_to_simulate] = g
+  end
+
+  opts.on('-t THREADS', '--threads THREADS', Integer, "Number of Threads to use [#{options[:number_of_threads]}]") do |t|
+    options[:number_of_threads] = t
+  end
+
+  opts.on_tail('-h', '--help', 'Show this message') do
+    puts opts
+    exit
+  end
+end
+
+opt_parser.parse!(ARGV)
+
+games_to_simulate = options[:games_to_simulate]
+number_of_threads = options[:number_of_threads]
+puts "Simulating #{games_to_simulate} games of chutes and ladders on #{number_of_threads} threads"
+
+threads = []
+number_of_threads.times do |i|
+  threads[i] = Thread.new { Thread.current[:results] = simulate_games(games_to_simulate/number_of_threads) }
+end
+
+thread_results = []
+frequency_of_turns = Array.new 151, 0
+threads.each do |t|
+  t.join
+  thread_results << t[:results]
+  thread_results[-1][:frequency_of_turns].each_index do |index|
+    frequency_of_turns[index] += thread_results[-1][:frequency_of_turns][index]
+  end
 end
 
 puts_histogram frequency_of_turns
-puts "#{number_of_wins} games won of #{games_simulated} simulated games"
+#puts "#{number_of_wins} games won of #{games_simulated} simulated games"
